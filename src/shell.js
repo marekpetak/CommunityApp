7993a9d9448719765feb7a99e88cc02a332f3d9e
Marek Petak <marekpetak@kannegiesser.com>
2017-05-24 14:15:35 +0100

    Basic routing done

--- src/shell.js
+++ src/shell.js
@@ -1,14 +1,31 @@
 export class Shell {
     constructor() {
-        
+
     }
 
-    configureRouter(config, router){
+    configureRouter(config, router) {
         this.router = router;
         config.title = "Capital Area .NEt USer Group";
-        config.map([
-            {route: '', moduleId: 'events'}
-        ]);
+        config.map([{
+            route: ['', 'events'],
+            moduleId: 'events/events',
+            name: 'Events',
+            title: 'Events',
+            nav: true
+        },
+        {
+            route: 'jobs',
+            moduleId: 'jobs/jobs',
+            name: 'Jobs',
+            title: 'Jobs',
+            nav: true
+        },{
+            route: 'discussion',
+            moduleId: 'discussion/discussion',
+            name: 'Discussion',
+            title: 'Discussion',
+            nav: true
+        }]);
 
     }
 }
